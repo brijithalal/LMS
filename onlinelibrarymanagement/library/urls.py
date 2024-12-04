f9a6c2fc4156ab8d742bc9a85e6b74d688898b89
brijitha <brijithalal36@gmail.com>
2024-12-04 21:10:02 +0530

    second commit login page

--- onlinelibrarymanagement/library/urls.py
+++ onlinelibrarymanagement/library/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.home,name='home_path')
+    path('',views.home,name='home_path'),
+       path('accounts/login/',views.user_login,name='login'),
 ]
--- onlinelibrarymanagement/library/views.py
+++ onlinelibrarymanagement/library/views.py
@@ -1,5 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+# from onlinelibrarymanagement.library.forms import MyLoginForm
+from django.shortcuts import redirect, render
+from .forms import MyLoginForm
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 def home(request):
     return render(request,'library/base.html')
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        #we will be getting username and password through post
+        login_form=MyLoginForm(request.POST)
+        if login_form.is_valid():
+
+            cleaned_data =login_form.cleaned_data
+            auth_user = authenticate(request,username = cleaned_data['username'],password=cleaned_data['password'])
+        if auth_user is not None:
+            login(request,auth_user)
+            #get the user's group name
+            group = auth_user.groups.first()
+            group_name = group.name if group else "No group"
+            request.session['group_name'] = group_name
+            # return HttpResponse('Authenticated')
+            return redirect('home_path')
+        
+        else:
+            return HttpResponse('Not Authenticated')
+    else:
+        #if error send form again
+        login_form=MyLoginForm()
+    return render(request,'library/login_form.html',
+                  {'login_form':login_form})
