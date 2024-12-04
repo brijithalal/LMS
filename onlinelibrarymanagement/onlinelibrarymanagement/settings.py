dd8958fececfe710c47b99c734299d3ccc92eaf7
brijitha <brijithalal36@gmail.com>
2024-12-04 12:55:35 +0530

    static committed

--- onlinelibrarymanagement/onlinelibrarymanagement/settings.py
+++ onlinelibrarymanagement/onlinelibrarymanagement/settings.py
@@ -119,18 +119,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT =os.path.join(BASE_DIR,'onlinelibrarymanagement', 'media')
 MEDIA_URL='/media/'
 #for setting a static image
 STATIC_URL ='/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'onlinelibrarymanagement', 'static')]
 
 
 
